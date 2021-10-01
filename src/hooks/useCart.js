639b6bae0995a35e338e3943b7efa1e12b455abc
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-10-01 10:58:23 +0600

    add custom hooks

---
+++ src/hooks/useCart.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { getStoredCart } from "../utilities/fakedb";
+
+const useCart = (products) => {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    if (products.length) {
+      const savedCart = getStoredCart();
+      console.log(savedCart)
+      const storeCart = [];
+      for (const key in savedCart) {
+        const addedProduct = products.find((product) => product.key === key);
+        if (addedProduct) {
+          addedProduct.quantity = savedCart[key];
+          storeCart.push(addedProduct);
+        }
+      }
+      setCart(storeCart);
+    }
+  }, [products]);
+
+  return [cart, setCart];
+};
+export default useCart;
