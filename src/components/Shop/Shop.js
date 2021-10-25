0a2f143cceba594f7811b01b077e806b02b2a5e3
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-10-25 16:32:48 +0600

    add pagination

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -11,14 +11,25 @@ const Shop = () => {
   // products to be rendered on the UI
   const [displayProducts, setDisplayProducts] = useState([]);
 
+  // pagination state
+  const [page, setPage] = useState(0)
+  const [pageCount, setPageCount] = useState(0);
+  const size = 10;
+
   useEffect(() => {
-    fetch("./products.json")
+    fetch(`http://localhost:5000/products?page=${page}&&size=${size}`)
       .then((res) => res.json())
       .then((data) => {
-        setProducts(data);
-        setDisplayProducts(data);
+        setProducts(data.products);
+        setDisplayProducts(data.products);
+
+        // page count of total products
+        const count = data.count;
+        const pageNumber = Math.ceil(count / size);
+        setPageCount(pageNumber);
+
       });
-  }, []);
+  }, [page]);
 
   useEffect(() => {
     if (products.length) {
@@ -68,12 +79,26 @@ const Shop = () => {
       <div className='search-container'>
         <input type='text' onChange={handleSearch} placeholder='Search Product' />
       </div>
+
       <div className='shop-container'>
         <div className='product-container'>
           {displayProducts.map((product) => (
             <Product key={product.key} product={product} handleAddToCart={handleAddToCart}></Product>
           ))}
+
+          <div className='pagination'>
+            {
+              [...Array(pageCount).keys()]
+                .map(number => <button
+                  className={number === page ? 'selected' : ''}
+                  key={number}
+                  onClick={() => setPage(number)}>
+                  {number + 1}
+                </button>)
+            }
+          </div>
         </div>
+
         <div className='cart-container'>
           <Cart cart={cart}>
             <Link to='/review'>
--- src/hooks/useProduct.js
+++ src/hooks/useProduct.js
@@ -5,12 +5,12 @@ const useProduct = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("./products.json")
+    fetch("http://localhost:5000/products")
       .then((res) => res.json())
-      .then((data) => setProducts(data));
+      .then((data) => setProducts(data.products));
   }, []);
 
-  return[products,setProducts]
+  return [products, setProducts]
 };
 
 export default useProduct;
