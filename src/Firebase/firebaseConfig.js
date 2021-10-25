e54e05a2ded55b195a72d26cb376b9a6f56967fa
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-10-25 09:02:42 +0600

    sequre api key

--- src/Firebase/firebaseConfig.js
+++ src/Firebase/firebaseConfig.js
@@ -1,10 +1,10 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyASCPeMTzTJ-sdaZUoFy74vhZDI425RHU4",
-    authDomain: "ema-john-simple-with-router.firebaseapp.com",
-    projectId: "ema-john-simple-with-router",
-    storageBucket: "ema-john-simple-with-router.appspot.com",
-    messagingSenderId: "1016169923901",
-    appId: "1:1016169923901:web:57dca11f0a25cec18e3154"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_API_ID,
 };
 export default firebaseConfig;
